e893343580ae606e30d96e92aa9b747f34da8a0f
zamanmohamed <zamanmohamed97@gmail.com>
2023-04-05 19:47:33 +0530

    before custom hook

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import "./App.css";
+
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+
+function App() {
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/:userId/places" element={<UserPlaces />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="/places/:placeId" element={<UpdatePlace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
